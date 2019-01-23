9eb73d0abe7941ba89a6eb735947f78da8c4a890
Harry Maindonald <harry.maindonald>
2019-01-23 15:49:28 +1300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
+    state = {
+        todos: [
+            {id: 1, content: 'Make the beds'},
+            {id: 2, content: 'Feed the dogs'}
+        ]
+    }
+
+    deleteTodo = (id) => {
+        const todos = this.state.todos.filter(todo => {
+            return todo.id !== id
+        });
+        this.setState({
+            todos
+        })
+    }
+
+    addTodo = (todo) => {
+        todo.id = Math.random();
+        let todos = [...this.state.todos, todo]
+        this.setState({
+            todos
+        })
+    }
+
+    
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Harry's Todo's</h1>
+        <h6 id="date" className="center blue-text text-darken-3">For: {new Date().toDateString()}</h6>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
